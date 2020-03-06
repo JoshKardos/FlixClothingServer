261b9b5c602eebd82ef826c5751dfc3535bba0ee
Josh Kardos <joshkardos@gmail.com>
2020-03-06 09:39:35 -0800

    demo

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
+const stripe = require('stripe')('sk_test_c8WpTVTZOQnrqvqWXJJLaTeQ00whht62Tb')
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
-
-express()
+const a = express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
