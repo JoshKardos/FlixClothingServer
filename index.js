542ba3f039e887b0b2b653af042407e42cb34103
Josh Kardos <joshkardos@gmail.com>
2020-04-20 20:08:59 -0700

    fix for full send

--- index.js
+++ index.js
@@ -51,4 +51,40 @@ app.post('/api/form', (req, res) => {
     })
   })
 })
+
+app.post('/fullsend/report/yak', (req, res) => {
+  const { designId, apiKey } = req.body 
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
+      <div style="border: 1px solid grey; padding:12px;">
+        <h1 style="text-align:center; text-decoration:underline">Flix Clothing</h3>
+        <h3 style="text-align:center; text-decoration:underline">A design was just reported by a user</h3>
+        <div style="text-align: center;">
+          <p>Design Id: ${designId} <p>
+        </div>
+      </div>
+    `
+    const transporter = nodemailer.createTransport(
+      nodemailerSendgrid({
+        apiKey: apiKey
+      })
+    )
+    const toEmail = 'joshkardos@gmail.com'
+    const mailOptions = {
+      from: 'Flix Clothing <joshkardos@gmail.com>',
+      to: toEmail,
+      replyTo: 'noreply@gmail.com',
+      subject: 'Reported design',
+      html: htmlEmail
+    }
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        res.status(500).end()
+        return
+      }
+      console.log('Email sent')
+      res.status(200).send()
+    })
+  })
+})
   
